a0b4c64e06dbb0ccbf982a05c43c397cef9ebc17
Raul Torres <raultorres2603@gmail.com>
2023-04-11 19:12:21 +0200

    s

--- src/components/ContextApp.jsx
+++ src/components/ContextApp.jsx
@@ -1,4 +1,5 @@
 import { createContext, useState } from "react";
+import { io } from "socket.io-client";
 
 export const appContext = createContext();
 
@@ -6,6 +7,8 @@ export function ContextProvider({ children }) {
   const [page, savePage] = useState(
     sessionStorage.getItem("user") ? "main" : "login"
   );
+
+  const socket = io("http://localhost:3000");
 
   return (
     <appContext.Provider value={{ page, savePage }}>

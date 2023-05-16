9f6266f336d6312c1ca4039180d074668c78b9b8
Raul Torres <raultorres2603@gmail.com>
2023-05-16 19:20:03 +0200

    GRAPHS NOT DONE

--- src/components/Facturation.jsx
+++ src/components/Facturation.jsx
@@ -1,14 +1,25 @@
 import { useState, useContext, useEffect } from "react";
 import { appContext } from "./ContextApp";
 import { socketContext } from "./ContextSocket";
-import ReactECharts from "echarts-for-react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+  BarChart,
+  Bar,
+} from "recharts";
 import { Alert } from "./Alert";
+import { Graphs } from "../lib/Graphs";
 
 export function Facturation() {
   const { setRequest, socketResponse, setSocketResponse } =
     useContext(socketContext);
-  const [chart, setChart] = useState(null);
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(null);
   const [from, setFrom] = useState(
     `${new Date().getFullYear()}-${
       new Date().getMonth() + 1
@@ -19,27 +30,6 @@ export function Facturation() {
       new Date().getMonth() + 1
     }-${new Date().getDate()}`
   );
-  const [xAxisData, setXAxisData] = useState(null);
-  const [options, setOptions] = useState({
-    title: {
-      text: `Facturation of ${from} to ${to}`,
-    },
-    tooltip: {
-      trigger: "axis",
-    },
-    legend: {
-      data: data,
-    },
-    grid: { top: 8, right: 8, bottom: 24, left: 36 },
-    xAxis: {
-      type: "category",
-      data: xAxisData,
-    },
-    yAxis: {
-      type: "value",
-    },
-    series: data,
-  });
 
   function handleSubmit(ev) {
     ev.preventDefault();
@@ -56,8 +46,28 @@ export function Facturation() {
     });
   }
 
+  function handleDate(ev) {
+    switch (ev.target.id) {
+      case "from":
+        setFrom(ev.target.value);
+        break;
+
+      case "to":
+        setTo(ev.target.value);
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  async function handleSearch(ev) {
+    let dataRes = await Graphs.getGraph(from, to);
+    setData(dataRes);
+  }
+
   return (
-    <div className="facturation-container" style={{ height: "100vh" }}>
+    <div className="facturation-container">
       <div className="row">
         <div className="display-4 mb-4 text-center">My Bill Menu</div>
       </div>
@@ -152,51 +162,73 @@ export function Facturation() {
         <div className="display-5 text-center">Filter Bills</div>
       </div>
       <div className="row">
-        <div className="col-6">
+        <div className="col-4">
           <div className="row">
-            <div class="input-group input-group-lg">
-              <span class="input-group-text" id="basic-addon1">
-                From
-              </span>
-              <input
-                type="date"
-                class="form-control"
-                id="from"
-                defaultValue={from}
-                aria-label="Username"
-                aria-describedby="basic-addon1"
-              />
+            <div className="col-6">
+              <div className="row">
+                <div class="input-group input-group-lg">
+                  <span class="input-group-text" id="basic-addon1">
+                    From
+                  </span>
+                  <input
+                    type="date"
+                    class="form-control"
+                    id="from"
+                    onChange={handleDate}
+                    defaultValue={from}
+                    aria-label="Username"
+                    aria-describedby="basic-addon1"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="col-6">
+              <div className="row">
+                <div class="input-group input-group-lg">
+                  <span class="input-group-text" id="basic-addon1">
+                    To
+                  </span>
+                  <input
+                    type="date"
+                    class="form-control"
+                    id="to"
+                    onChange={handleDate}
+                    defaultValue={to}
+                    aria-label="Username"
+                    aria-describedby="basic-addon1"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="row mt-4">
+                <div className="row">
+                  <button
+                    type="button"
+                    class="btn btn-success align-middle fs-4"
+                    onClick={handleSearch}
+                  >
+                    Buscar
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-        <div className="col-6">
+        <div className="col-8">
           <div className="row">
-            <div class="input-group input-group-lg">
-              <span class="input-group-text" id="basic-addon1">
-                To
-              </span>
-              <input
-                type="date"
-                class="form-control"
-                id="to"
-                defaultValue={to}
-                aria-label="Username"
-                aria-describedby="basic-addon1"
-              />
-            </div>
+            <ResponsiveContainer width={"100%"} height={300}>
+              <BarChart data={data}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar type="monotone" dataKey="value" fill="#8884d8" />
+              </BarChart>
+            </ResponsiveContainer>
           </div>
         </div>
-      </div>
-      <div className="row mt-4">
-        <div className="row">
-          <button type="button" class="btn btn-success align-middle fs-4">
-            Buscar
-          </button>
-        </div>
-      </div>
-      <hr />
-      <div className="row">
-        <ReactECharts option={options} />
       </div>
     </div>
   );
---
+++ src/lib/Graphs.js
@@ -0,0 +1,59 @@
+import axios from "axios";
+
+export class Graphs {
+  constructor() {}
+
+  static async getGraph(from, to) {
+    let response = await axios.get(
+      `${import.meta.env.VITE_PROTOCOL}://${import.meta.env.VITE_DOMAIN}:${
+        import.meta.env.VITE_PORT
+      }/graphs/${from}/${to}`
+    );
+
+    return response.data;
+  }
+
+  static getMonth(month) {
+    switch (month) {
+      case 1:
+        return "Jan";
+        break;
+      case 2:
+        return "Febr";
+        break;
+      case 3:
+        return "Mar";
+        break;
+      case 4:
+        return "Apr";
+        break;
+      case 5:
+        return "May";
+        break;
+      case 6:
+        return "Jun";
+        break;
+      case 7:
+        return "Jul";
+        break;
+      case 8:
+        return "Aug";
+        break;
+      case 9:
+        return "Sep";
+        break;
+      case 10:
+        return "Oct";
+        break;
+      case 11:
+        return "Nov";
+        break;
+      case 12:
+        return "Dec";
+        break;
+
+      default:
+        break;
+    }
+  }
+}
